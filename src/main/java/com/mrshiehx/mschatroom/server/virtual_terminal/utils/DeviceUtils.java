6700fd17642b9b4bb18c51b78a3bee9da385dc3e
MrShieh-X <3553413882@qq.com>
2021-05-26 18:20:18 +0800

    20210526, deleted the directory virtual_terminal

--- src/main/java/com/mrshiehx/mschatroom/server/virtual_terminal/utils/DeviceUtils.java
+++
@@ -1,8 +0,0 @@
-package com.mrshiehx.mschatroom.server.virtual_terminal.utils;
-
-public class DeviceUtils {
-    private DeviceUtils(){}
-    public static String getDeviceName(){
-        return "msc";
-    }
-}
